34646a3e2360f3c062bb080b0dce57cfb72b36d5
The1Swan N/A <isabella.ann.swan@gmail.com>
2024-10-28 17:45:49 +0000

    decimal to binary convertion

---
+++ converter.py
@@ -0,0 +1,21 @@
+def decimaltobinary(num):   
+    if num < 0:
+        print("None")
+    elif num == 0:
+        print("0000")
+    
+    binary = 0
+    placement = 1
+
+    while num > 0:
+        remainder = num % 2
+        num //= 2
+        binary += remainder * placement
+        placement *= 10
+        
+
+    return binary
+
+num = int(input("What number do you want to convert?"))
+print(decimaltobinary(num))
+
