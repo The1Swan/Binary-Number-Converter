eec72ca3bf2408114c598f967b3a3bc52693b15e
The1Swan N/A <isabella.ann.swan@gmail.com>
2024-10-28 17:59:58 +0000

    binary to number 0-15 converter

--- converter.py
+++ converter.py
@@ -1,21 +1,39 @@
 def decimaltobinary(num):   
-    if num < 0:
-        print("None")
-    elif num == 0:
+    if num == 0:
         print("0000")
-    
-    binary = 0
-    placement = 1
+    elif num == 1:
+        print("0001")
+    elif num == 2:
+        print("0010")    
+    elif num == 3:
+        print("0011")    
+    elif num == 4:
+        print("0100")    
+    elif num == 5:
+        print("0101")    
+    elif num == 6:
+        print("0110")    
+    elif num == 7:
+        print("0111")    
+    elif num == 8:
+        print("1000")    
+    elif num == 9:
+        print("1001")    
+    elif num == 10:
+        print("1010")    
+    elif num == 11:
+        print("1011")    
+    elif num == 12:
+        print("1100")    
+    elif num == 13:
+        print("1101")    
+    elif num == 14:
+        print("1110")    
+    elif num == 15:
+        print("1111")    
+    elif num > 15:
+        print("N/A")
 
-    while num > 0:
-        remainder = num % 2
-        num //= 2
-        binary += remainder * placement
-        placement *= 10
-        
-
-    return binary
-
-num = int(input("What number do you want to convert?"))
+num = int(input("What number between 0 - 15 do you want to convert?"))
 print(decimaltobinary(num))
 
